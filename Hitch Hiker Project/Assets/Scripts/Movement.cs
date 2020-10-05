28e41296e6c3d9a0f0715c8e5e1cfee29ff15349
Jackson <Jacksonlee21@comcast.net>
2020-10-05 13:34:51 -0700

    Basic Controls

---
+++ Hitch Hiker Project/Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    float movedirection = 0f;
+
+    void Update()
+    {
+        if(Input.GetKey("a"))
+        {
+            movedirection = -1f;
+        }
+        else if (Input.GetKey("d"))
+        {
+            movedirection = 1f;
+        }
+        else
+        {
+            movedirection = 0f;
+        }
+
+    }
+
+    public void FixedUpdate()
+    {
+        if (movedirection == -1)
+        {
+            Debug.Log("Left");
+            transform.position += new Vector3(-1 * Time.deltaTime, 0, 0);
+        }
+        if (movedirection == 1)
+        {
+            Debug.Log("Right");
+            transform.position += new Vector3(1 * Time.deltaTime, 0, 0);
+        }
+    }
+}

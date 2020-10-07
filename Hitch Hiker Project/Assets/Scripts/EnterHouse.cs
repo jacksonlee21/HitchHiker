c1c9facdef65233ada0c1acc27546804d12488ce
Luke <lukewoodland2003@gmail.com>
2020-10-07 10:35:37 -0700

    WIP Entering Building
    Working on a system to enter buildings, right now the trigger collider is not working so it does not know when it is at a door.

---
+++ Hitch Hiker Project/Assets/Scripts/EnterHouse.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterHouse : MonoBehaviour
+{
+    private bool atDoor;
+    private string houseName;
+
+    private void Start()
+    {
+        atDoor = false;
+    }
+
+    private void Update()
+    {
+        if (atDoor)
+        {
+            Debug.Log(houseName);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("EnterBuilding"))
+        {
+            Debug.Log("blah");
+            atDoor = true;
+            houseName = collision.GetComponentInParent<Transform>().tag;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("EnterBuilding"))
+        {
+            atDoor = false;
+        }
+    }
+}

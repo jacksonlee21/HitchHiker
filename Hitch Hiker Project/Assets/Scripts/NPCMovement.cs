3d44b8c7b67e36619ed2b82a5fbc6736ed743ec5
Luke <lukewoodland2003@gmail.com>
2020-10-10 17:23:32 -0700

    NPC Random Wandering
    I created  a script for NPC's to wander randomly. Right now it is using the Main Character sprite but has a yellow color to test out animation.

---
+++ Hitch Hiker Project/Assets/Scripts/NPCMovement.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMovement : MonoBehaviour
+{
+    private Vector3 randPos;
+    public float speed = 5f;
+    public float walkTime;
+    private float timeTillGetPos;
+
+    private Animator anim;
+    private Rigidbody2D rb;
+
+    private bool NextToPlayer;
+    [HideInInspector]
+    public bool TalkingToPlayer;
+
+    private void Start()
+    {
+        timeTillGetPos = walkTime;
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+
+        NextToPlayer = false;
+        TalkingToPlayer = false;
+    }
+
+    void Update()
+    {
+        if (NextToPlayer)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                TalkingToPlayer = true;
+                anim.SetBool("In Motion", false);
+            }
+        }
+
+        if (!TalkingToPlayer)
+        {
+            GoToPos();
+            TriggerAnimation();
+        }
+    }
+
+    void TriggerAnimation()
+    {
+        if (transform.position == randPos)
+        {
+            anim.SetBool("In Motion", false);
+        }
+        else if (transform.position.x < randPos.x)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            anim.SetBool("In Motion", true);
+        }
+        else if (transform.position.x > randPos.x)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            anim.SetBool("In Motion", true);
+        }
+    }
+
+    void GoToPos()
+    {
+        timeTillGetPos += Time.deltaTime;
+
+        if(timeTillGetPos >= walkTime)
+        {
+            GetRandPos();
+            timeTillGetPos = 0;
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, randPos, speed * Time.deltaTime);
+        }
+    }
+
+    void GetRandPos()
+    {
+        float randX = Random.Range(-9f, 9f);
+
+        randPos = new Vector3(randX, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            NextToPlayer = true;
+        }
+    }
+}

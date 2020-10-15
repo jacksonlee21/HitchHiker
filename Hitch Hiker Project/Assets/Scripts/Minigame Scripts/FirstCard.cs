2df3fec9260ebf1c5ce8eea3f1ed28ff82016892
Jackson <Jacksonlee21@comcast.net>
2020-10-15 13:45:55 -0700

    First 2 Minigames

---
+++ Hitch Hiker Project/Assets/Scripts/Minigame Scripts/FirstCard.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstCard : MonoBehaviour
+{
+    int Sequence;
+    float time;
+
+    public void Start()
+    {
+        Sequence = GetComponentInParent<Minigame2s>().Sequence;
+        Debug.Log(Sequence);
+    }
+
+
+    public void FixedUpdate()
+    {
+        time += Time.deltaTime;
+        if (Sequence == 1)
+        {
+            //ends at 2
+            if (time < 1f)
+            {
+                transform.position += new Vector3(-2 * Time.deltaTime, 0, 0);
+            }
+            else if (time < 2f)
+            {
+                transform.position += new Vector3(-2 * Time.deltaTime, 0, 0);
+            }
+            else if (time < 3f)
+            {
+                transform.position += new Vector3(0 * Time.deltaTime, 0, 0);
+            }
+            else if (time < 4f)
+            {
+                transform.position += new Vector3(2 * Time.deltaTime, 0, 0);
+            }
+        }
+        if (Sequence == 2)
+        {
+            //ends at 3
+            if (time < 1f)
+            {
+                transform.position += new Vector3(-2 * Time.deltaTime, 0, 0);
+            }
+            else if (time < 2f)
+            {
+                transform.position += new Vector3(0 * Time.deltaTime, 0, 0);
+            }
+            else if (time < 3f)
+            {
+                transform.position += new Vector3(0 * Time.deltaTime, 0, 0);
+            }
+            else if (time < 4f)
+            {
+                transform.position += new Vector3(2 * Time.deltaTime, 0, 0);
+            }
+        }
+        if (Sequence == 3)
+        {
+            //ends at 1
+            if (time < 1f)
+            {
+                transform.position += new Vector3(-2 * Time.deltaTime, 0, 0);
+            }
+            else if (time < 2f)
+            {
+                transform.position += new Vector3(-2 * Time.deltaTime, 0, 0);
+            }
+            else if (time < 3f)
+            {
+                transform.position += new Vector3(2 * Time.deltaTime, 0, 0);
+            }
+            else if (time < 4f)
+            {
+                transform.position += new Vector3(-2 * Time.deltaTime, 0, 0);
+            }
+        }
+        if (Sequence == 4)
+        {
+            //ends at 1
+            if (time < 1f)
+            {
+                transform.position += new Vector3(-2 * Time.deltaTime, 0, 0);
+            }
+            else if (time < 2f)
+            {
+                transform.position += new Vector3(2 * Time.deltaTime, 0, 0);
+            }
+            else if (time < 3f)
+            {
+                transform.position += new Vector3(-2 * Time.deltaTime, 0, 0);
+            }
+            else if (time < 4f)
+            {
+                transform.position += new Vector3(-2 * Time.deltaTime, 0, 0);
+            }
+        }
+    }
+}
---
+++ Hitch Hiker Project/Assets/Scripts/Minigame Scripts/Minigame1s.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minigame1s : MonoBehaviour
+{
+    int counter = 0;
+    public int limit;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Space"))
+        {
+            counter++;
+        }
+        Debug.Log(counter);
+
+        if(counter == limit)
+        {
+            //You Win;
+        }
+    }
+}

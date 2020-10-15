ed25b3d619acc3712f9d09603fd9c7fc5ad4dfd8
Luke <lukewoodland2003@gmail.com>
2020-10-14 19:18:17 -0700

    Added comments

--- Hitch Hiker Project/Assets/Scripts/NPCTalk.cs
+++ Hitch Hiker Project/Assets/Scripts/NPCTalk.cs
@@ -24,32 +24,39 @@ public class NPCTalk : MonoBehaviour
 
     private void Start()
     {
+        //Sets private vars
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         npcController = GameObject.FindGameObjectWithTag("NPCController").GetComponent<NPCController>();
         anim = GetComponent<Animator>();
-
+        //Sets bools false
         NextToPlayer = false;
         TalkingToPlayer = false;
     }
 
     void Update()
     {
+        //Checks if NPC has a task for player
         if (HasTask)
         {
-            StartTalking();
+            //If yes -> runs function
+            ReadyToTalk();
         }
     }
 
-    void StartTalking()
+    //Function that checks if player presses space to talk
+    private void ReadyToTalk()
     {
         if (NextToPlayer)
         {
+            //Checks if space is pressed to start talking to NPC
             if (Input.GetKeyDown(KeyCode.Space))
             {
+                //Shows text and sets talking to player bool to true, and next to player false
                 TalkingToPlayer = true;
+                NextToPlayer = false;
                 npcController.ShowText();
-                NextToPlayer = false;  
             }
+            //Stops NPC walking animation
             if (TalkingToPlayer)
             {
                 anim.SetBool("In Motion", false);
@@ -59,12 +66,17 @@ public class NPCTalk : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        //Checks if other NPC is next to player, and if game is in task mode
         if(other.CompareTag("Player") && isOtherNPC && npcController.inTask)
         {
+            //Spawns finished task effect
             Instantiate(npcController.FinishedTaskEffect, player.position, Quaternion.identity);
+            //Ends task mode and gets rid of NPC role
             isOtherNPC = false;
             npcController.inTask = false;
-            Destroy(transform.GetChild(0).gameObject);
+            //Gets rid of image and text
+            transform.GetChild(0).gameObject.SetActive(false);
+            transform.GetChild(0).transform.SetParent(null);
             npcController.NPCText.gameObject.SetActive(false);
         }
     }

f8dad5f9e4722bb012dc3576060ceda4ab2978f4
Jackson <Jacksonlee21@comcast.net>
2020-10-16 14:07:43 -0700

    Finished Game

--- Hitch Hiker Project/Assets/Scripts/Minigame Scripts/RightCard.cs
+++ Hitch Hiker Project/Assets/Scripts/Minigame Scripts/RightCard.cs
@@ -6,10 +6,12 @@ public class RightCard : MonoBehaviour
 {
     int Sequence;
     float time;
+    Material color;
 
     public void Start()
     {
         Sequence = GetComponentInParent<Minigame2s>().Sequence;
+        color = GetComponent<Renderer>().material;
         Debug.Log(Sequence);
     }
 
@@ -17,22 +19,48 @@ public class RightCard : MonoBehaviour
     public void FixedUpdate()
     {
         time += Time.deltaTime;
-        if (Sequence == 1)
+        if(time < 2)
         {
-            //ends at 1
-            if (time < 1f)
+            if (time < .4f)
             {
-                transform.position += new Vector3(2 * Time.deltaTime, 0, 0);
+                color.color = Color.red;
+            }
+            else if(time < .8f)
+            {
+                color.color = Color.green;
+            }
+            else if (time < 1.2f)
+            {
+                color.color = Color.red;
+            }
+            else if (time < 1.6f)
+            {
+                color.color = Color.green;
             }
             else if (time < 2f)
             {
-                transform.position += new Vector3(-2 * Time.deltaTime, 0, 0);
+                color.color = Color.red;
             }
-            else if (time < 3f)
+        }
+
+
+
+        if (Sequence == 1)
+        {
+            //ends at 1
+            if (time < 3f && time > 2f)
+            {
+                transform.position += new Vector3(2 * Time.deltaTime, 0, 0);
+            }
+            else if (time < 4f && time > 3f)
             {
                 transform.position += new Vector3(-2 * Time.deltaTime, 0, 0);
             }
-            else if (time < 4f)
+            else if (time < 5f && time > 4f)
+            {
+                transform.position += new Vector3(-2 * Time.deltaTime, 0, 0);
+            }
+            else if (time < 6f && time > 5f)
             {
                 transform.position += new Vector3(0 * Time.deltaTime, 0, 0);
             }
@@ -40,19 +68,19 @@ public class RightCard : MonoBehaviour
         if (Sequence == 2)
         {
             //ends at 2
-            if (time < 1f)
+            if (time < 3f && time > 2f)
             {
                 transform.position += new Vector3(2 * Time.deltaTime, 0, 0);
             }
-            else if (time < 2f)
+            else if (time < 4f && time > 3f)
             {
                 transform.position += new Vector3(-2 * Time.deltaTime, 0, 0);
             }
-            else if (time < 3f)
+            else if (time < 5f && time > 4f)
             {
                 transform.position += new Vector3(-2 * Time.deltaTime, 0, 0);
             }
-            else if (time < 4f)
+            else if (time < 6f && time > 5f)
             {
                 transform.position += new Vector3(2 * Time.deltaTime, 0, 0);
             }
@@ -60,19 +88,19 @@ public class RightCard : MonoBehaviour
         if (Sequence == 3)
         {
             //ends at 3
-            if (time < 1f)
+            if (time < 3f && time > 2f)
             {
                 transform.position += new Vector3(-2 * Time.deltaTime, 0, 0);
             }
-            else if (time < 2f)
+            else if (time < 4f && time > 3f)
             {
                 transform.position += new Vector3(0 * Time.deltaTime, 0, 0);
             }
-            else if (time < 3f)
+            else if (time < 5f && time > 4f)
             {
                 transform.position += new Vector3(2 * Time.deltaTime, 0, 0);
             }
-            else if (time < 4f)
+            else if (time < 6f && time > 5f)
             {
                 transform.position += new Vector3(2 * Time.deltaTime, 0, 0);
             }
@@ -80,19 +108,19 @@ public class RightCard : MonoBehaviour
         if (Sequence == 4)
         {
             //ends at 2
-            if (time < 1f)
+            if (time < 3f && time > 2f)
             {
                 transform.position += new Vector3(0 * Time.deltaTime, 0, 0);
             }
-            else if (time < 2f)
+            else if (time < 4f && time > 3f)
             {
                 transform.position += new Vector3(0 * Time.deltaTime, 0, 0);
             }
-            else if (time < 3f)
+            else if (time < 5f && time > 4f)
             {
                 transform.position += new Vector3(-2 * Time.deltaTime, 0, 0);
             }
-            else if (time < 4f)
+            else if (time < 6f && time > 5f)
             {
                 transform.position += new Vector3(2 * Time.deltaTime, 0, 0);
             }

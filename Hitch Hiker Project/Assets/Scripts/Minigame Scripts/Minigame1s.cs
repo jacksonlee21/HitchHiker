706002cc63d6a0e877a462a583d7f2dc0d19fe04
Jackson <Jacksonlee21@comcast.net>
2020-10-19 13:00:53 -0700

    Added Hammer Animation

--- Hitch Hiker Project/Assets/Scripts/Minigame Scripts/Minigame1s.cs
+++ Hitch Hiker Project/Assets/Scripts/Minigame Scripts/Minigame1s.cs
@@ -6,12 +6,22 @@ public class Minigame1s : MonoBehaviour
 {
     int counter = 0;
     public int limit;
+    int direction = 3;
+    GameObject Hammer;
 
     void Update()
     {
         if(Input.GetButtonDown("Space"))
         {
             counter++;
+            if(counter % 20 == 0)
+            {
+                direction = direction * -1;
+                //Debug.Log("Test");
+            }
+            //Debug.Log(direction);
+            transform.Rotate(Vector3.back * direction); 
+            
         }
         Debug.Log(counter);
 

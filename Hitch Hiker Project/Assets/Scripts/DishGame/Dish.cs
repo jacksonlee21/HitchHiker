1e9068b02eb0f8f0af87b09777479e52a6ef5472
Luke <lukewoodland2003@gmail.com>
2020-10-23 14:37:23 -0700

    StartedDishGame
    I starting making a minigame where you clean dishes. Right now you scrub dishes and once it is clean a new dirty plate slides into screen.

---
+++ Hitch Hiker Project/Assets/Scripts/DishGame/Dish.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dish : MonoBehaviour
+{
+    public Sprite[] dishSprites;
+    private SpriteRenderer sr;
+    [HideInInspector]
+    public bool isClean;
+    private bool inPos;
+    [HideInInspector]
+    public int cleanCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isClean = false;
+        inPos = false;
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!inPos)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Vector2.zero, .5f);
+            inPos = transform.position == Vector3.zero;
+        }
+        else
+        {
+            sr.sprite = dishSprites[cleanCounter];
+        }
+    }
+
+    public void MoveOutOfScene()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(15,0), .75f);
+    }
+}
---
+++ Hitch Hiker Project/Assets/Scripts/DishGame/DishManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DishManager : MonoBehaviour
+{
+    public GameObject dishPrefab;
+    private Dish dish;
+    private int DishesCleaned;
+    //private Transform dishLocation;
+
+    private Vector3 spawnPos = new Vector3(-15, 0, 0);
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        DishesCleaned = 0;
+        CreateDish();
+    }
+
+    private void Update()
+    {
+        if(dish.cleanCounter == 9)
+        {
+            DishesCleaned++;
+            StartCoroutine(GetRidOfDish());
+        }
+    }
+
+    IEnumerator GetRidOfDish()
+    {
+        dish.MoveOutOfScene();
+        yield return new WaitForSeconds(.3f);
+        Destroy(dish.gameObject);
+        CreateDish();
+    }
+
+    private void CreateDish()
+    {
+        GameObject dishSpawned = Instantiate(dishPrefab, spawnPos, Quaternion.identity);
+        dish = dishSpawned.GetComponent<Dish>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Sponge"))
+        {
+            dish.cleanCounter++;
+            dish.cleanCounter = Mathf.Clamp(dish.cleanCounter, 0, 9);
+        }
+    }
+}

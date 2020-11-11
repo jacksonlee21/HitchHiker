3ebbdfb19614f7d0817289bb498bb612804931e0
Matthew <dowling95m@comcast.net>
2020-11-10 21:46:35 -0800

    Added Sprites and Click and Drag

---
+++ Hitch Hiker Project/Assets/Scripts/ClickAndDrag.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickAndDrag : MonoBehaviour
+{
+
+    private float startPosX;
+    private float startPosY;
+    private bool isBeingHeld = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBeingHeld)
+        {
+            Vector3 mousePos;
+            //make and set mouse position to its position on screen
+            mousePos = Input.mousePosition;
+
+            //convert the screen mouse position to in game position
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, 0);
+        }
+    }
+
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButton(0))
+        {
+
+            Vector3 mousePos;
+            //make and set mouse position to its position on screen
+            mousePos = Input.mousePosition;
+
+            //convert the screen mouse position to in game position
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+
+            //calculates the difference between the middle of the object and where you click so mouse stays where you click
+            startPosX = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+
+            isBeingHeld = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        isBeingHeld = false;
+    }
+    
+}
---
+++ Hitch Hiker Project/Assets/Scripts/TrashSorting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashSorting : MonoBehaviour
+{
+
+    public GameObject[] Items;
+    int rand;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rand = Random.Range(0, 6);
+        Items[rand].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+}

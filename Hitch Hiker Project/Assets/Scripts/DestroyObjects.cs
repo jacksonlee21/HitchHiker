2ce3503b9405a42fb925d9e6e976f7714cd0ec12
Matthew <dowling95m@comcast.net>
2020-11-11 13:23:50 -0800

    Started the Destroying

---
+++ Hitch Hiker Project/Assets/Scripts/DestroyObjects.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D (Collision2D col)
+    {
+      
+        col.gameObject.SetActive(false);
+        col.gameObject.transform.position = new Vector2(0f, 2f);
+        Debug.Log("destroy please");
+
+    }
+
+}
--- Hitch Hiker Project/Assets/Scripts/TrashSorting.cs
+++ Hitch Hiker Project/Assets/Scripts/TrashSorting.cs
@@ -7,7 +7,8 @@ public class TrashSorting : MonoBehaviour
 
     public GameObject[] Items;
     int rand;
-
+    int amountActive = 0;
+    int startAmount = 0;
 
 
     // Start is called before the first frame update
@@ -18,10 +19,35 @@ public class TrashSorting : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    /*void Update()
     {
+        //check every object in the array
+        for(int i = 0; i < Items.Length; i++)
+        {
+            
+            if(i == 0)
+            {
+                startAmount = amountActive;
+            }
 
-    }
+            if(Items[i].activeSelf)
+            {
+                amountActive++;
+            }
+
+            if(i == Items.Length - 1 && startAmount == amountActive)
+            {
+                amountActive = 0;
+            }
+        }
+
+        if(amountActive < 1)
+        {
+            rand = Random.Range(0, 6);
+            Items[rand].SetActive(true);
+            amountActive = 0;
+        }
+    }*/
 
 
 }

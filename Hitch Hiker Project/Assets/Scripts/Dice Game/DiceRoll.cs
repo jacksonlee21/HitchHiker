e5e98b9bc71be588abd5bca35193235e767131bb
Jackson <Jacksonlee21@comcast.net>
2020-11-17 14:08:35 -0800

    Dice Game is Done

--- Hitch Hiker Project/Assets/Scripts/Dice Game/DiceRoll.cs
+++ Hitch Hiker Project/Assets/Scripts/Dice Game/DiceRoll.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DiceRoll : MonoBehaviour
 {
@@ -10,7 +11,9 @@ public class DiceRoll : MonoBehaviour
     int counter;
     int SelectedRoll;
     int Bet;
+    int multiplyer;
     int Stagetracker = 0;
+    float timer;
 
     //Dice Sprites
     public GameObject[] DieOne;
@@ -21,6 +24,18 @@ public class DiceRoll : MonoBehaviour
 
     //Value Select Buttons
     public GameObject SelectButtons;
+
+    //Exit Button
+    public GameObject Exit;
+
+    //Money Check Text
+    public GameObject MoneyCheck;
+
+    //SFX
+    public AudioSource StageChange;
+    public AudioSource Winner;
+    public AudioSource Loser;
+    public AudioSource RollSFX;
 
     void Start()
     {
@@ -34,6 +49,8 @@ public class DiceRoll : MonoBehaviour
         {
             BettingButtons.SetActive(false);
             SelectButtons.SetActive(true);
+            Exit.SetActive(false);
+            MoneyCheck.SetActive(false);
         }
         if(Stagetracker == 2)
         {
@@ -51,80 +68,143 @@ public class DiceRoll : MonoBehaviour
         {
             PostGame();
         }
+        if(Stagetracker == 4)
+        {
+            timer += Time.deltaTime;
+            if(timer > 5)
+            {
+                SceneManager.LoadScene("DialogueSystem");
+            }
+        }
+    }
+
+    //Exit game if you have no money
+    public void LeaveGame()
+    {
+        SceneManager.LoadScene("DialogueSystem");
     }
     
     //Betting Buttons
     public void ButtonPressTen()
     {
-        Bet = 10;
-        Stagetracker++;
+        if(PlayerPrefs.GetInt("cMoney") < 10)
+        {
+            MoneyCheck.SetActive(true);
+        }
+        else
+        {
+            Bet = 10;
+            Stagetracker++;
+            StageChange.Play();
+            PlayerPrefs.SetInt("cMoney", PlayerPrefs.GetInt("cMoney") - 10);
+        }    
     }
     public void ButtonPressTwentyFive()
     {
-        Bet = 25;
-        Stagetracker++;
+        if (PlayerPrefs.GetInt("cMoney") < 25)
+        {
+            MoneyCheck.SetActive(true);
+        }
+        else
+        {
+            Bet = 25;
+            Stagetracker++;
+            StageChange.Play();
+            PlayerPrefs.SetInt("cMoney", PlayerPrefs.GetInt("cMoney") - 25);
+        }
     }
     public void ButtonPressFifty()
     {
-        Bet = 50;
-        Stagetracker++;
+        if (PlayerPrefs.GetInt("cMoney") < 50)
+        {
+            MoneyCheck.SetActive(true);
+        }
+        else
+        {
+            Bet = 50;
+            Stagetracker++;
+            StageChange.Play();
+            PlayerPrefs.SetInt("cMoney", PlayerPrefs.GetInt("cMoney") - 50);
+        }
     }
 
     //Select Buttons
     public void SelectDiceRoll2()
     {
         SelectedRoll = 2;
+        multiplyer = 36;
+        StageChange.Play();
         Stagetracker++;
     }
     public void SelectDiceRoll3()
     {
         SelectedRoll = 3;
+        multiplyer = 18;
+        StageChange.Play();
         Stagetracker++;
     }
     public void SelectDiceRoll4()
     {
         SelectedRoll = 4;
+        multiplyer = 12;
+        StageChange.Play();
         Stagetracker++;
     }
     public void SelectDiceRoll5()
     {
         SelectedRoll = 5;
+        multiplyer = 9;
+        StageChange.Play();
         Stagetracker++;
     }
     public void SelectDiceRoll6()
     {
         SelectedRoll = 6;
+        multiplyer = 7;
+        StageChange.Play();
         Stagetracker++;
     }
     public void SelectDiceRoll7()
     {
         SelectedRoll = 7;
+        multiplyer = 6;
+        StageChange.Play();
         Stagetracker++;
     }
     public void SelectDiceRoll8()
     {
         SelectedRoll = 8;
+        multiplyer = 7;
+        StageChange.Play();
         Stagetracker++;
     }
     public void SelectDiceRoll9()
     {
         SelectedRoll = 9;
+        multiplyer = 9;
+        StageChange.Play();
         Stagetracker++;
     }
 
     public void SelectDiceRoll10()
     {
         SelectedRoll = 10;
+        multiplyer = 12;
+        StageChange.Play();
         Stagetracker++;
     }
     public void SelectDiceRoll11()
     {
         SelectedRoll = 11;
+        multiplyer = 18;
+        StageChange.Play();
         Stagetracker++;
     }
     public void SelectDiceRoll12()
     {
         SelectedRoll = 12;
+        multiplyer = 36;
+        StageChange.Play();
         Stagetracker++;
     }
 
@@ -135,6 +215,8 @@ public class DiceRoll : MonoBehaviour
 
         diceRollOne = Random.Range(1,7);
         diceRollTwo = Random.Range(1,7);
+
+        RollSFX.Play();
 
         DieOne[diceRollOne - 1].SetActive(true);
         DieTwo[diceRollTwo - 1].SetActive(true);
@@ -153,11 +235,15 @@ public class DiceRoll : MonoBehaviour
         {
             //Win
             Debug.Log("Winner");
-            //Bet * Multiplier
+            PlayerPrefs.SetInt("cMoney", PlayerPrefs.GetInt("cMoney") + (Bet * multiplyer));
+            Winner.Play();
+            Stagetracker++;
         }
         else
         {
             Debug.Log("Loser");
+            Loser.Play();
+            Stagetracker++;
             //Lose
         }
     }

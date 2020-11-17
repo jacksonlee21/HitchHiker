b900fac988b320b0bad6fa742db2c388185dd2c8
Luke <lukewoodland2003@gmail.com>
2020-11-17 14:36:18 -0800

    WIP Deleting arrows
    I am working on a script to delete arrows after the player enters a house, this is so the player is only able to enter one house at a time.

---
+++ Hitch Hiker Project/Assets/Scripts/ArrowManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowManager : MonoBehaviour
+{
+    public GameObject[] arrows;
+    private bool[] arrowActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(GameObject.FindGameObjectWithTag("EnterBuilding") != null)
+            arrows = GameObject.FindGameObjectsWithTag("EnterBuilding");
+    }
+
+    private void Update()
+    {
+        if (GameObject.FindGameObjectWithTag("EnterBuilding") != null)
+        {
+            int index = 0;
+            foreach (var arrow in arrows)
+            {
+                arrow.SetActive(!arrowActive[index]);
+                index++;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    public void CheckArrow(GameObject arrowAtPlayer)
+    {
+        int index = 0;
+        foreach (var arrow in arrows)
+        {
+            if(arrowAtPlayer = arrow)
+            {
+                arrowActive[index] = true;
+            }
+            index++;
+        }
+    }
+}

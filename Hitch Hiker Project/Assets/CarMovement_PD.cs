419f4ca6cb20b835468032abbc936f3a4a7928d9
Luke <lukewoodland2003@gmail.com>
2020-11-25 13:11:48 -0800

    Gameplay Finished
    I created a minigame where you need to drive down the road to deliver a pizza by dodging cars. It is top down and uses simple movement with three lanes to drive it.

---
+++ Hitch Hiker Project/Assets/CarMovement_PD.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement_PD : MonoBehaviour
+{
+    private Vector3 endPosition;
+
+    [SerializeField]
+    private float speed = 5;
+
+    private void Start()
+    {
+        endPosition = transform.position - new Vector3(0,20,0);
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, endPosition, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Finish"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Hitch Hiker Project/Assets/CarSpawner_PD.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner_PD : MonoBehaviour
+{
+    public GameObject carPrefab;
+    public Transform[] spawnPoints;
+
+    [SerializeField]
+    private float spawnFrequency = 1;
+    private float timer = 0;
+
+    [HideInInspector]
+    public bool gameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = spawnFrequency;
+        gameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= spawnFrequency && !gameOver)
+        {
+            int randSpawnIndex = Random.Range(0, 3);
+            Vector3 randSpawnPoint = spawnPoints[randSpawnIndex].position;
+            Instantiate(carPrefab, randSpawnPoint, carPrefab.transform.rotation);
+            timer = 0;
+        }
+    }
+}

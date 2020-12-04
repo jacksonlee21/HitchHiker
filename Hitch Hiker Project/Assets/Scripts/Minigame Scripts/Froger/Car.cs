feea44e15809016a63c3368c0347e7210decb1b7
giuliano <giulianoscasso@gmail.com>
2020-12-04 13:09:14 -0800

    New Main with Frogger

--- Hitch Hiker Project/Assets/Scripts/Minigame Scripts/Froger/Car.cs
+++ Hitch Hiker Project/Assets/Scripts/Minigame Scripts/Froger/Car.cs
@@ -1,26 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class Car : MonoBehaviour
 {
-
+ 
     public float moveSpeed = 5f;
     public bool moveD = false;
         void Start()
     {
         
     }
-
+ 
     
     void Update()
     {
         Vector2 pos = transform.localPosition;
-
+ 
         if(moveD)
         {
             pos.x += Vector2.right.x * moveSpeed * Time.deltaTime;
-
+ 
                 if(pos.x >= 15)
             {
                 pos.x = -15;
@@ -35,9 +35,9 @@ public class Car : MonoBehaviour
                 pos.x = 15;
             }
         }
-
+ 
         
-
+ 
         transform.localPosition = pos;
     }
 }

f9fe4a14b47fd7464f2b5b4bf974fb808c2e431b
giuliano <giulianoscasso@gmail.com>
2020-12-16 14:13:30 -0800

    Idk anymore

--- Hitch Hiker Project/Assets/Scripts/UI/DayNightCycle.cs
+++ Hitch Hiker Project/Assets/Scripts/UI/DayNightCycle.cs
@@ -10,40 +10,44 @@ public class DayNightCycle : MonoBehaviour
     public TextMeshProUGUI Clock;
     private float startTime;
     public SpriteRenderer Sky;
-    float c = 1;
+    float c = 1f;
     float t = 0;
 
     float x;
     string minutes;
     string seconds;
     float rT;
+    float dayL;
 
     // Start is called before the first frame update
     void Start()
     {
         startTime = Time.time;
+        dayL = 35f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        t += Time.deltaTime;
-        rT = Mathf.Round(t);
+        t += Time.deltaTime*dayL;
 
         
-        minutes = ((int) t/6).ToString();
-        seconds = (t%6).ToString("f0");
-        x = (t%6);
+        minutes = ((int) t/60).ToString();
+        seconds = (t%60).ToString("f0");
+        x = (t%60);
 
-        Clock.text = minutes + ":" + seconds + "0";
+        Clock.text = minutes + ":" + seconds;
 
-        if(t/6f > 6)
+        if(t/60 > 6)
         {
-            if(x > 6)
-            c -= 1f / 6f;
-            Sky.color = new Color(c,c,c,255f);
-            Debug.Log(c);
-            x = 0;
+            if(x > 30f)
+            {
+                c -= 1f / 60f;
+                Vector4 CC = new Vector4 (c,c,c,255f);
+                Sky.color = CC;
+                Debug.Log(c);
+                x = 0;
+            }
         }
         
     }

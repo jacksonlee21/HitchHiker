2afdd104a23b7fcf27ef968061fc3f273a3c7558
Jackson <Jacksonlee21@comcast.net>
2020-12-16 14:19:15 -0800

    New Me

--- Hitch Hiker Project/Assets/Scripts/Karma.cs
+++ Hitch Hiker Project/Assets/Scripts/Karma.cs
@@ -6,17 +6,20 @@ public class Karma : MonoBehaviour
 {
     public float Karmalevel;
     public float tips;
+
     public float dailyLuck;
+    float lowPoint;
+    float highPoint;
 
     private void Update()
     {
-        Karmalevel = PlayerPrefs.GetFloat("playerKarma");
+        //Karmalevel = PlayerPrefs.GetFloat("playerKarma");
 
-        if (PlayerPrefs.GetFloat("playerKarma") > 10)
+        if (Karmalevel > 10)
         {
             PlayerPrefs.SetFloat("playerKarma", 10);
         }
-        if (PlayerPrefs.GetFloat("playerKarma") < -10f)
+        if (Karmalevel < -10f)
         {
             PlayerPrefs.SetFloat("playerKarma", -10f);
         }
@@ -26,14 +29,24 @@ public class Karma : MonoBehaviour
 
         //when you reach a new checkpoint in the karma system a little message will play to let the player know
         SelfEsteem();
+
+        if(Input.GetKeyDown("k"))
+        {
+            luckLevel();
+        }
+
     }
 
     void TipJar()
     {
-        if (PlayerPrefs.GetFloat("playerKarma") > 2.5f)
+        if (Karmalevel >= 2.5f)
         {
-            tips = Mathf.Round((1 + (PlayerPrefs.GetFloat("playerKarma") / 15)) * 100) / 100;
+            tips = Mathf.Round((1 + (Karmalevel / 15)) * 100) / 100;
             //this divides your karma level by 15, adds 1, and rounds it to the second decimal, giving you a 1.xx number you can multiply the base wage from working by.
+        }
+        else
+        {
+            tips = 1;
         }
     }
 
@@ -43,34 +56,37 @@ public class Karma : MonoBehaviour
         Dialogue dialogueScript = dialogueManager.GetComponent<Dialogue>();
 
         //Good Karma
-        if (PlayerPrefs.GetFloat("playerKarma") > 7.5f && PlayerPrefs.GetFloat("KarmaCheckpoint") != 3)
+        if (Karmalevel > 7.5f && PlayerPrefs.GetFloat("KarmaCheckpoint") != 3)
         {
-            //dialogueScript.NewText(new string[] { "You are a good person" });
+            //dialogueScript.NewText(new string[] { "You are a good person" }, false, new string[] {"", ""});
             PlayerPrefs.SetFloat("KarmaCheckpoint", 3);
+            Debug.Log("Good 3");
         }
-        if (PlayerPrefs.GetFloat("playerKarma") > 5 && PlayerPrefs.GetFloat("KarmaCheckpoint") != 2)
+        else if (Karmalevel >= 5 && PlayerPrefs.GetFloat("KarmaCheckpoint") != 2)
         {
             //dialogueScript.NewText(new string[] { "You feel good" });
+            Debug.Log("Good 2");
             PlayerPrefs.SetFloat("KarmaCheckpoint", 2);
         }
-        if (PlayerPrefs.GetFloat("playerKarma") > 2.5f && PlayerPrefs.GetFloat("KarmaCheckpoint") != 1)
+        else if (Karmalevel > 2.5f && PlayerPrefs.GetFloat("KarmaCheckpoint") != 1)
         {
             //dialogueScript.NewText(new string[] { "You feel a spring in your step" });
+            Debug.Log("Good 1");
             PlayerPrefs.SetFloat("KarmaCheckpoint", 1);
         }
 
         //Bad Karma
-        if (PlayerPrefs.GetFloat("playerKarma") < -2.5f && PlayerPrefs.GetFloat("KarmaCheckpoint") != -1f)
+        if (Karmalevel < -2.5f && PlayerPrefs.GetFloat("KarmaCheckpoint") != -1f)
         {
             //dialogueScript.NewText(new string[] { "You feel the weight of your mistakes" });
             PlayerPrefs.SetFloat("KarmaCheckpoint", -1f);
         }
-        if (PlayerPrefs.GetFloat("playerKarma") < -5f && PlayerPrefs.GetFloat("KarmaCheckpoint") != -2f)
+        if (Karmalevel < -5f && PlayerPrefs.GetFloat("KarmaCheckpoint") != -2f)
         {
             //dialogueScript.NewText(new string[] { "You feel like garbage" });
             PlayerPrefs.SetFloat("KarmaCheckpoint", -2f);
         }
-        if (PlayerPrefs.GetFloat("playerKarma") < -7.5f && PlayerPrefs.GetFloat("KarmaCheckpoint") != -3f)
+        if (Karmalevel < -7.5f && PlayerPrefs.GetFloat("KarmaCheckpoint") != -3f)
         {
             //dialogueScript.NewText(new string[] { "You are garbage" });
             PlayerPrefs.SetFloat("KarmaCheckpoint", -3f);
@@ -79,9 +95,19 @@ public class Karma : MonoBehaviour
 
     void luckLevel()
     {
-        //float lowPoint = (PlayerPrefs.GetFloat("playerKarma") + 10) - ;
-        //float highPoint = (PlayerPrefs.GetFloat("playerKarma") + 10) + ;
-        //dailyLuck = Random.Range(lowPoint, highPoint);
+        if(Karmalevel != 0)
+        {
+            lowPoint = (Karmalevel + 10) - (4 / Mathf.Abs(Karmalevel));
+            highPoint = (Karmalevel + 10) + (4 / Mathf.Abs(Karmalevel));
+        }
+        if(Karmalevel == 0)
+        {
+            lowPoint = (Karmalevel + 10) - 4;
+            highPoint = (Karmalevel + 10) + 4;
+        }
+        dailyLuck = Random.Range(lowPoint, highPoint);
+        dailyLuck = dailyLuck - 10;
+        Debug.Log(dailyLuck);
     }
 
     void JobComplete()

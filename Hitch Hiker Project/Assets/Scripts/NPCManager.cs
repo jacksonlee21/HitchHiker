07c7219dd2f6da410e6cbea89ed984a988f78b74
Luke Woodland <lukewoodland2003@gmail.com>
2021-01-16 01:23:17 -0800

    Added Arrows To NPCs

---
+++ Hitch Hiker Project/Assets/Scripts/NPCManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCManager : MonoBehaviour
+{
+    public GameObject spaceToTalkObject;
+    void Start()
+    {
+        spaceToTalkObject.SetActive(false);
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("NPC"))
+        {
+            spaceToTalkObject.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("NPC"))
+        {
+            spaceToTalkObject.SetActive(false);
+        }
+    }
+}

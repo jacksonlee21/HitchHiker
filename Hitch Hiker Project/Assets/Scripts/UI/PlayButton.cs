6b9596748fb0d191c2891c6017a8f0fa7f08c73a
Jackson <Jacksonlee21@comcast.net>
2021-01-19 16:35:21 -0800

    The main menu now fades to black when pressing play

--- Hitch Hiker Project/Assets/Scripts/UI/PlayButton.cs
+++ Hitch Hiker Project/Assets/Scripts/UI/PlayButton.cs
@@ -2,18 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayButton : MonoBehaviour
 {
+    public Image blackFade;
+    public GameObject Fade;
+    bool buttonpress;
+    public float fadePace = 0;
+
+   
     public void GotoMainScene()
     {
         PlayerPrefs.DeleteAll();
-        SceneManager.LoadScene("Town1");
-        
+        Fade.SetActive(true);
+        buttonpress = true;
     }
 
     public void GotoMenuScene()
     {
         SceneManager.LoadScene("NewMenu");
     }
+
+    private void Update()
+    {
+        if(buttonpress == true)
+        {
+            fadePace = fadePace + .0025f;
+            blackFade.color = new Color(blackFade.color.r, blackFade.color.g, blackFade.color.b, fadePace);
+            if(fadePace > 1.5)
+            {
+                SceneManager.LoadScene("Town1");
+            }
+        }
+    }
+
+
+
+
+
 }
